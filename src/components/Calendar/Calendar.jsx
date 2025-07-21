d27e8f1f1c65952237e58cdffcf665036052c724
EvGus52 <evgenijguskov247@gmail.com>
2025-07-21 22:07:01 +0300

    создал файлы для компонентов

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,11 +1,1 @@
-function Calendar() {
-  return (
-    <div className="calendar">
-      {/* Здесь будет календарь задачи */}
-      <p className="calendar__ttl subttl">Даты</p>
-      {/* ...дополнительная разметка календаря... */}
-    </div>
-  );
-}
 
-export default Calendar;
