24f4e2d7de40648614b7852f10e852bc81e04ad6
EvGus52 <evgenijguskov247@gmail.com>
2025-07-21 22:14:07 +0300

    компоненты готовы

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,1 +1,11 @@
+function Calendar() {
+  return (
+    <div className="calendar">
+      {/* Здесь будет календарь задачи */}
+      <p className="calendar__ttl subttl">Даты</p>
+      {/* ...дополнительная разметка календаря... */}
+    </div>
+  );
+}
 
+export default Calendar;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+function Main({ children }) {
+  return (
+    <main className="main _hide">
+      <div className="main__block">
+        <div className="main__content">{children}</div>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
