482c13454fc49454d0afabeea17d486497fef790
EvGus52 <evgenijguskov247@gmail.com>
2025-08-30 21:51:58 +0300

    Цветовые темы собраны в объект

---
+++ src/Colors.js
@@ -0,0 +1,18 @@
+export const Colors = {
+  orange: {
+    background: "#ffe4c2",
+    color: "#ff6d00",
+  },
+  green: {
+    background: "#b4fdd1",
+    color: "#06b16e",
+  },
+  purple: {
+    background: "#e9d4ff",
+    color: "#9a48f1",
+  },
+  gray: {
+    background: "#94a6be",
+    color: "#ffffff",
+  },
+};
